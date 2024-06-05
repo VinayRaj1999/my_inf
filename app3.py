4092aaff42b96d4afe165e8783afe43c673a8659
VinayRaj1999 <Vinayraj291999@gmail.com>
2024-06-05 15:02:58 +0530

    Add files via upload

---
+++ app3.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.write ("im vinay raj i completed my ugc in fisheries science college of magalore. MBA in food business in dairy science college Hebbal, Bangalore")
+         
+
+
